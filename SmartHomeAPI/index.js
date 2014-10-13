96f7e9aae7263b4e9368d940e73f71575a2f61da
Jason Pollman <jppuffnstuff@gmail.com>
2014-10-13 16:06:06 -0400

    Strange GIT Problem Fixed

--- SmartHomeAPI/index.js
+++ SmartHomeAPI/index.js
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-(function() { 
-  var API = require("./APICore/SmartHomeAPI.js");
-})();
-=======
 (function() { require('./APICore/SmartHomeAPI.js'); })();
->>>>>>> 19749284de5835e68a70b6432ee4c8629ef0aefd
-
